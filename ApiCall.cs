2103967a6d998102ebbc72ef0c815240d8f9fe88
Flaudz <nico936d@edu.campusvejle.dk>
2020-09-08 11:45:48 +0200

    Add project files.

---
+++ ApiCall.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using static WpfApp1.ApiGetSet;
+
+namespace WpfApp1
+{
+    public class ApiCall
+    {
+        public Root getInfo(string city)
+        {
+            var json = new WebClient().DownloadString($"http://api.openweathermap.org/data/2.5/find?q={city}&units=metric&APPID=050936d1aac5cc35d9f3df894ba7f990");
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+            return myDeserializedClass;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp1.ApiGetSet;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApiCall Apicall = new ApiCall();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string city = cityInput.Text;
+            Root weather = Apicall.getInfo(city);
+            
+            foreach (var item in weather.list)
+            {
+                weatherText.Text = $"Temperaturen er: {Math.Round(item.main.temp).ToString()}";
+                foreach (var ting in item.weather)
+                {
+                    if(ting.main == "Clouds")
+                    {
+                        Bg.ImageSource = new BitmapImage(new Uri(@"C:\Users\nico936d\Documents\S-2\Uge_37\Tirsdag_VejrAPI\VejrAPI\WpfApp1\Assets\BackgroundClouds.jpg"));
+                    } else if(ting.description == "few clouds")
+                    {
+                        Bg.ImageSource = new BitmapImage(new Uri(@"C:\Users\nico936d\Documents\S-2\Uge_37\Tirsdag_VejrAPI\VejrAPI\WpfApp1\Assets\BackgroundSunClouds.jpg"));
+                    }
+                }
+            }
+        }
+    }
+}
